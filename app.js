ea29ecad63e35fb766a3f9f8a9b79aa4f2dfa64e
Ykat <ykat@Yekaterinas-MacBook-Pro.local>
2017-10-11 20:56:00 -0700

    final cleanup

--- app.js
+++ app.js
@@ -1,84 +1,78 @@
 
 const state = {
-    photos : [],
-    geojson: {},
-    currentLat: 55.754093,
-    currentLong: 37.6173,
-    randomLocations:[
+photos : [],
+geojson: {},
+currentLat: 55.754093,
+currentLong: 37.6173,
+randomLocations:[
+    {
+    "name": "Paris, France",
+    long: 2.352222,
+    lat: 48.856614
+    },
+    {
+    "name": "Berlin, Germany",
+    long: 13.404954,
+    lat: 52.520007 
+    },
+    {  
+    "name": "New York, New York",
+    long: -115.174556,
+    lat: 36.1023717
+    },
         {
-        "name": "Paris, France",
-        long: 2.352222,
-        lat: 48.856614
-        },
+    "name": "Oahu, Hawaii",
+    long: -158.000057,
+    lat: 21.438912 
+    },
         {
-        "name": "Berlin, Germany",
-        long: 13.404954,
-        lat: 52.520007 
-        },
-        {  
-        "name": "New York, New York",
-        long: -115.174556,
-        lat: 36.1023717
-        },
-            {
-        "name": "Oahu, Hawaii",
-        long: -158.000057,
-        lat: 21.438912 
-        },
-            {
-        "name": "Sacramento, California",
-        long: -158.000057,
-        lat: 21.438912 
-        },
-           {
-        "name": "Athens, Greece",
-        long: 23.727539,
-        lat: 37.983810
-        },
-        {
-            "name": "San Paolo, Brazil",
-            long: -46.633309,
-            lat: -23.550520
-        },
-         {
-            "name": "Istanbul, Turkey",
-            long: 28.978359,
-            lat: 41.008238
-        },
-         {
-            "name": "Marrakesh, Morroco",
-            long: -7.981084,
-            lat: 31.629472
-        },
-         {
-            "name": "Bucaramanga, Colombia",
-            long: -73.122742,
-            lat: 7.119349
-        },
-        {
-            "name": "Seattle, Washington",
-            long: -122.332071,
-            lat: 47.606209
-        },
-          {
-            "name": "Las Vegas, Nevada",
-            long: -115.139830,
-            lat: 36.169941
-        },
+    "name": "Sacramento, California",
+    long: -158.000057,
+    lat: 21.438912 
+    },
+       {
+    "name": "Athens, Greece",
+    long: 23.727539,
+    lat: 37.983810
+    },
+    {
+        "name": "San Paolo, Brazil",
+        long: -46.633309,
+        lat: -23.550520
+    },
+     {
+        "name": "Istanbul, Turkey",
+        long: 28.978359,
+        lat: 41.008238
+    },
+     {
+        "name": "Marrakesh, Morroco",
+        long: -7.981084,
+        lat: 31.629472
+    },
+     {
+        "name": "Bucaramanga, Colombia",
+        long: -73.122742,
+        lat: 7.119349
+    },
+    {
+        "name": "Seattle, Washington",
+        long: -122.332071,
+        lat: 47.606209
+    },
+      {
+        "name": "Las Vegas, Nevada",
+        long: -115.139830,
+        lat: 36.169941
+    },
 
-        {
-            "name": "Las Vegas, Nevada",
-            long: -115.139830,
-            lat: 36.169941
-        },
+    {
+        "name": "Las Vegas, Nevada",
+        long: -115.139830,
+        lat: 36.169941
+    },
 
-
-    ]
-    
-}
-
-
-mapboxgl.accessToken = 'pk.eyJ1IjoieWthdGVzcXVlIiwiYSI6ImNqN3V1bmphMjRlN3YyeHBrbDV0cmYyZzkifQ.IkEhhVc-aWVnuLsnNlf5Zg';
+]}
 
 
 let map = new mapboxgl.Map({
@@ -86,14 +80,16 @@ let map = new mapboxgl.Map({
     style: 'mapbox://styles/ykatesque/cj86z79ws33k12ro5es8c7yqx',
     center: [37.6173, 55.754093],
     zoom: 5,
-    // causes pan & zoom handlers not to be applied, similar to
-    // .dragging.disable() and other handler .disable() funtions in Leaflet.
 });
 
 
-let geocoder =  new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken
-});
+
+mapboxgl.accessToken = 'pk.eyJ1IjoieWthdGVzcXVlIiwiYSI6ImNqN3V1bmphMjRlN3YyeHBrbDV0cmYyZzkifQ.IkEhhVc-aWVnuLsnNlf5Zg';
+
+
+
+
+let geocoder =  new MapboxGeocoder({accessToken: mapboxgl.accessToken});
 
 map.addControl(geocoder);
 
@@ -114,20 +110,18 @@ map.on('load', function() {
             "circle-radius": 10,
             "circle-color": "#007cbf"
         }
-    });
+});
 
-    // Listen for the `geocoder.input` event that is triggered when a user
-    // makes a selection and add a symbol that matches the result.
-    geocoder.on('result', function(ev) {
-        $('#map').removeClass('half-map');
-        $('.detail-info-container').addClass('hidden');
-        let coord = ev.result.geometry.coordinates;
-        let geolong = coord[1];
-        let geolat = coord[0];
-        map.getSource('single-point').setData(ev.result.geometry);
-        getDataFromFlickr(geolong, geolat, makeGeoJson);
-        
-
+// Listen for the `geocoder.input` event that is triggered when a user
+// makes a selection and add a symbol that matches the result.
+geocoder.on('result', function(ev) {
+    $('#map').removeClass('half-map');
+    $('.detail-info-container').addClass('hidden');
+    let coord = ev.result.geometry.coordinates;
+    let geolong = coord[1];
+    let geolat = coord[0];
+    map.getSource('single-point').setData(ev.result.geometry);
+    getDataFromFlickr(geolong, geolat, makeGeoJson);
     });
 });
 
@@ -140,61 +134,51 @@ function getDataFromFlickr(lat, lon, callback) {
     state.currentLat = lat,
     state. currentLong = lon
     const settings = {
-    url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a6342f1b1a90efed3c5276f7ea8bd15a&lat=' + lat + '&lon='+ lon + '&extras=geo%2C+owner_name&format=json&nojsoncallback=1',
-    dataType: 'json',
-    type: 'GET',
-    success: callback,
-    error: errorLog
-  };
-  console.log('GETTING DATA')
-  $.ajax(settings).done(function(data) {
-    state.photos = [];
-    result = data.photos.photo.slice(0, 100)
-    console.log(result.length)
-    if (result.length > 0) {
-        state.photos = result.map(item => ({
-        photo_url: 'https://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_z.jpg',
-        thumb_url: 'https://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_q.jpg',
-        flickr_url: 'https://flickr.com/' + item.owner + '/' + item.id,
-        author_url: 'https://www.flickr.com/photos/'+ item.owner,
-        author_id: item.owner,
-        title: item.title,
-        farm: item.farm,
-        author_name: item.ownername,
-        lat: item.latitude,
-        long: item.longitude,
-        photo_id: item.id,
-        server: item.server,
-        secret: item.secret,
-        }));
-        console.log(state.photos)
-        makeGeoJson(state.photos);
-
-    }
-      else {
+        url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a6342f1b1a90efed3c5276f7ea8bd15a&lat=' + lat + '&lon='+ lon + '&extras=geo%2C+owner_name&format=json&nojsoncallback=1',
+        dataType: 'json',
+        type: 'GET',
+        success: callback,
+        error: errorLog
+    };
+    $.ajax(settings).done(function(data) {
         state.photos = [];
-        noResults();
-      }
+        let result = data.photos.photo.slice(0, 100)
+        if (result.length > 0) {
+            state.photos = result.map(item => ({
+            photo_url: 'https://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_z.jpg',
+            thumb_url: 'https://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_q.jpg',
+            flickr_url: 'https://flickr.com/' + item.owner + '/' + item.id,
+            author_url: 'https://www.flickr.com/photos/'+ item.owner,
+            author_id: item.owner,
+            title: item.title,
+            farm: item.farm,
+            author_name: item.ownername,
+            lat: item.latitude,
+            long: item.longitude,
+            photo_id: item.id,
+            server: item.server,
+            secret: item.secret,
+            }));
+            makeGeoJson(state.photos);
+
+        }
+        else {
+            state.photos = [];
+            noResults();
+          }
 
     });
-
-
-
 }
+
 
 
 function errorLog(){
-    console.log('error')
+    var error;
 }
 
-
-
-
 function noResults(){
-    console.log('no results');
     state.photos = [];
     $('#map').append(`<div class="no-results">No Results at this Location. <div class="try-again">Please try again.</div></div>`)
-
 }
 
 
@@ -204,17 +188,15 @@ function noResults(){
 
 
 function makeGeoJson(){
-    console.log('in make geojson')
-
-      state.geojson = {
-      "type": "FeatureCollection",
-      "features": [],
-   };
-   if (state.photos.length > 0) {
-       state.photos.forEach(function(x){
-    state.geojson.features.push({ 
-     type: "Feature",
-      properties: {
+    state.geojson = {
+        "type": "FeatureCollection",
+        "features": [],
+    };
+    if (state.photos.length > 0) {
+        state.photos.forEach(function(x){
+        state.geojson.features.push({ 
+        type: "Feature",
+        properties: {
         name: x.title,
         thumb_url: x.thumb_url,
         author_name: x.author_id,
@@ -227,52 +209,35 @@ function makeGeoJson(){
           x.lat,
         ]
       } })
-   });
-   }
+      });
+    }
+    state.geojson.features.forEach(function(marker) {
+        var el = document.createElement('div');
+        el.className = 'marker';
+        new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML(`<figure class="effect-goliath"><img src="${marker.properties.thumb_url}" alt="img23"/>
+                        <figcaption><p>View Details</p><a href="#" class="view-link">View more</a></figcaption></figure>`)).addTo(map);
 
-
- state.geojson.features.forEach(function(marker) {
-      var el = document.createElement('div');
-      el.className = 'marker';
-    new mapboxgl.Marker(el)
-      .setLngLat(marker.geometry.coordinates)
-      .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-      .setHTML(`<figure class="effect-goliath">
-                        <img src="${marker.properties.thumb_url}" alt="img23"/>
-                        <figcaption>
-                            <p>View Details</p>
-                            <a href="#" class="view-link">View more</a>
-                        </figcaption>           
-                    </figure>`))
-
-        // `<h3> ${marker.properties.name} </h3> <div class="img-container"><img src="${marker.properties.thumb_url}"><span class="more-link">MORE INFO</span></div>`))
-      // .setHTML('<h3>' + marker.properties.name + '</h3><img src="' + marker.properties.thumb_url + '>'))
-      .addTo(map);
     });  
 
-  
 }
 
 
 function displayDetailData(obj){
-    let url = obj.flickr_url
-    let author_url  
-
+    let url = obj.flickr_url;
+    let author_url = obj.author_url;
     $('.flickr-link').attr("href", url);
+    $('.author-link').attr('href', author_url);
     $('.single-data').empty().append(`<h5>${obj.title}</h5><div class="large-img"><img src="${obj.photo_url}"></div>`);
-
-
-
 }
 
 
 
 function findPhotoByKey(array, key, value) {
-    console.log('findPhot')
     for (var i = 0; i < array.length; i++) {
         if (array[i][key] === value) {
-            console.log('found it');
-            console.log(array[i]);
             displayDetailData(array[i])
             }
     }
@@ -287,9 +252,8 @@ function geoLocateAndCallAPI() {
         lat: 55.754093,
         long: 37.6173,
     };
-
     if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(pos, options) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
              const currLocation = {
                 lat: pos.coords.latitude,
                 long: pos.coords.longitude,
@@ -299,15 +263,14 @@ function geoLocateAndCallAPI() {
                 zoom: 12,
             });
             getDataFromFlickr(currLocation.lat, currLocation.long)
-
         }, function() {
-            console.log('rendering default map')
+            getDataFromFlickr(defaultLocation.lat, defaultLocation.long);
         })
-    } else {
-        console.log('locating not working doing call with default location');
-
+    } 
+    else {
+        getDataFromFlickr(defaultLocation.lat, defaultLocation.long);
     }
-    
+
 }
 
 geoLocateAndCallAPI();
@@ -316,29 +279,22 @@ geoLocateAndCallAPI();
 $('#random-location').on('click', function (e) {
     e.preventDefault();
     var randomlocation = state.randomLocations[Math.floor(Math.random()*state.randomLocations.length)];
-    let lat = randomlocation.lat
-    let long = randomlocation.long
-    state.photos = []
+    let lat = randomlocation.lat;
+    let long = randomlocation.long;
+    state.photos = [];
     $('.detail-info-container').addClass('hidden');
     map.flyTo({center: [long, lat],  zoom: 12});
     getDataFromFlickr(lat, long);
-    // let lat =   (-74.50 + (Math.random() - 0.5) * 10); 
-    // let long =  (40 + (Math.random() - 0.5) * 10);
-    // state.photos = [];
-    // map.flyTo({center: [lat, long,],  zoom: 12});
-    // getDataFromFlickr(lat, long, makeGeoJson);
-    
+
 });
 
 
 $('body').on('click', '.effect-goliath', function(e){
-  e.preventDefault();
-  $('#map').addClass('half-map');
-  $('.detail-info-container').removeClass('hidden');
-  let img = $(this).parent().find( "img" )[0].src
-  findPhotoByKey(state.photos, "thumb_url", img)
-
-
+    e.preventDefault();
+    $('#map').addClass('half-map');
+    $('.detail-info-container').removeClass('hidden');
+    let img = $(this).parent().find( "img" )[0].src;
+    findPhotoByKey(state.photos, "thumb_url", img);
 });
 
 
